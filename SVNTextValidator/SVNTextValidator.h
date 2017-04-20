f9fc6aa06bf9d4e54e3e191ab66fc7c7e90ad74a
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-20 12:29:24 -0700

    Initial Commit

---
+++ SVNTextValidator/SVNTextValidator.h
@@ -0,0 +1,19 @@
+//
+//  SVNTextValidator.h
+//  SVNTextValidator
+//
+//  Created by Aaron Dean Bikis on 4/20/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNTextValidator.
+FOUNDATION_EXPORT double SVNTextValidatorVersionNumber;
+
+//! Project version string for SVNTextValidator.
+FOUNDATION_EXPORT const unsigned char SVNTextValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNTextValidator/PublicHeader.h>
+
+
