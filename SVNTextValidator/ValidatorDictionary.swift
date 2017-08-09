032cae130dadd8feeb0f460cdde3a9c575139171
Aaron Bikis <aaron.bikis@gmail.com>
2017-08-09 12:08:31 -0700

    add functionality to get id from key from ValidatorDictionary

--- SVNTextValidator/ValidatorDictionary.swift
+++ SVNTextValidator/ValidatorDictionary.swift
@@ -10,37 +10,41 @@ import Foundation
 
 
 public struct ValidatorDictionary<T> : Sequence {
-    
-    private var innerDictionary: [ObjectIdentifier: T] = [:];
-    
-    public subscript(key: ValidatableField?) -> T? {
-        get {
-            if let key = key {
-                return innerDictionary[ObjectIdentifier(key)];
-            } else {
-                return nil;
-            }
-        }
-        set(newValue) {
-            if let key = key {
-                innerDictionary[ObjectIdentifier(key)] = newValue;
-            }
-        }
+  
+  private var innerDictionary: [ObjectIdentifier: T] = [:];
+  
+  public subscript(key: ValidatableField?) -> T? {
+    get {
+      if let key = key {
+        return innerDictionary[ObjectIdentifier(key)];
+      } else {
+        return nil;
+      }
     }
-    
-    public mutating func removeAll() {
-        innerDictionary.removeAll()
+    set(newValue) {
+      if let key = key {
+        innerDictionary[ObjectIdentifier(key)] = newValue;
+      }
     }
-    
-    public mutating func removeValueForKey(_ key: ValidatableField) {
-        innerDictionary.removeValue(forKey: ObjectIdentifier(key))
-    }
-    
-    public var isEmpty: Bool {
-        return innerDictionary.isEmpty
-    }
-    
-    public func makeIterator() -> DictionaryIterator<ObjectIdentifier ,T> {
-        return innerDictionary.makeIterator()
-    }
+  }
+  
+  public mutating func removeAll() {
+    innerDictionary.removeAll()
+  }
+  
+  public mutating func removeValueForKey(_ key: ValidatableField) {
+    innerDictionary.removeValue(forKey: ObjectIdentifier(key))
+  }
+  
+  public var isEmpty: Bool {
+    return innerDictionary.isEmpty
+  }
+  
+  public func makeIterator() -> DictionaryIterator<ObjectIdentifier ,T> {
+    return innerDictionary.makeIterator()
+  }
+  
+  public func getObject(forID id: ObjectIdentifier) -> T? {
+      return innerDictionary[id]
+  }
 }
