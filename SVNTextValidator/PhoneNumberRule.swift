ecf7cde25ddd5b1bfa3043eb754a4b614e0dd999
Aaron Bikis <aaron.bikis@gmail.com>
2017-08-18 13:10:09 -0700

    update phonenumber field to be optional

--- SVNTextValidator/PhoneNumberRule.swift
+++ SVNTextValidator/PhoneNumberRule.swift
@@ -11,18 +11,27 @@ import Foundation
  `PhoneNumberRule` is a subclass of Rule that defines how a phone number is validated.
  */
 public class PhoneNumberRule: RegexRule {
-    
-    /// Phone number regular express string to be used in validation.
-    static let regex = "^\\d{3}-\\d{3}-\\d{4}$"
-    
-    /**
-     Initializes a `PhoneNumberRule` object. Used to validate that a field has a valid phone number.
-     
-    - parameter message: Error message that is displayed if validation fails.
-    - returns: An initialized `PasswordRule` object, or nil if an object could not be created for some reason that would not result in an exception. 
-    */
-    public convenience init(message : String = "Invalid Phone Number") {
-        self.init(regex: PhoneNumberRule.regex, message : message)
+  
+  /// Phone number regular express string to be used in validation.
+  static let regex = "^\\d{3}-\\d{3}-\\d{4}$"
+  
+  var isOptional: Bool = false
+  
+  /**
+   Initializes a `PhoneNumberRule` object. Used to validate that a field has a valid phone number.
+   
+   - parameter message: Error message that is displayed if validation fails.
+   - returns: An initialized `PasswordRule` object, or nil if an object could not be created for some reason that would not result in an exception.
+   */
+  public convenience init(message : String = "Invalid Phone Number", isOptional: Bool = false) {
+    self.init(regex: PhoneNumberRule.regex, message: message)
+    self.isOptional = isOptional
+  }
+  
+  public override func validate(_ value: String) -> Bool {
+    if value == "" && isOptional {
+      return true
     }
-    
+    return super.validate(value)
+  }
 }
